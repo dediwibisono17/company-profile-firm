52f5f9d4f452590f3b2d698f9355338f67c23ea3
Dedi Arief Wibisono <dedi.wibisono@Dedis-MacBook-Pro.local>
2025-07-28 15:50:52 +0700

    update

---
+++ js/why.js
@@ -0,0 +1,15 @@
+// alert(1)
+$.ajax({
+    url: '/company-profile-firm/js/why.json',
+    // url: 'js/why.json',
+    method: 'GET',
+    success: (response => {
+        console.log(response); 
+        var a;
+        for (a=0; a<response.length; a++) {
+            var paragraph = response[a].desc;
+            // var name = response[a].name;
+            $(".whyus .paragraph").append(paragraph)    
+        }
+    })
+})
