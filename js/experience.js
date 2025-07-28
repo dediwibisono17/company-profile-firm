ce1d46bb4ff9188b0f578061f886ba32da1505d9
Dedi Arief Wibisono <dedi.wibisono@Dedis-MacBook-Pro.local>
2025-07-28 14:11:13 +0700

    update

---
+++ js/experience.js
@@ -0,0 +1,18 @@
+// alert(1)
+$.ajax({
+    url: '/company-profile-firm/js/experience.json',
+    // url: 'js/experience.json',
+    method: 'GET',
+    success: (response => {
+        // console.log(response); 
+        var a;
+        for (a=0; a<response.length; a++) {
+            var name = response[a].name;
+            $(".wrap-3-col").append(`
+                    <div class="cardc">
+                        <h4>${name}</h4>
+                    </div>
+                `)    
+        }
+    })
+})
