0f9e3bc7ea96e571525bb32d2f8962d200cf03e6
Dedi Arief Wibisono <dedi.wibisono@Dedis-MacBook-Pro.local>
2025-08-01 15:07:33 +0700

    update

--- js/experience.js
+++ js/experience.js
@@ -1,18 +1,56 @@
 // alert(1)
+var baseURL = "company-profile-firm"
 $.ajax({
-    url: '/company-profile-firm/js/experience.json',
+    url: '/'+baseURL+'/js/experience.json',
+    // url: 'js/experience.json',
     // url: 'js/experience.json',
     method: 'GET',
     success: (response => {
         // console.log(response); 
-        var a;
-        for (a=0; a<response.length; a++) {
-            var name = response[a].name;
-            $(".wrap-3-col").append(`
+        // var data = (response[0].data);
+
+        $(".click-flag").click(function () {
+            var localS = (localStorage.getItem('lang'));
+            var indonesia = localS == "id";
+            var inggris = localS == "en";
+            $(".wrap-3-col").html("")
+            if (inggris) {
+                var data = response[1].data;
+                var a;
+                // console.log(data);
+                
+                for (a = 0; a < data.length; a++) {
+                    var name = data[a].name;
+                    $(".wrap-3-col").append(`
                     <div class="cardc">
                         <h4>${name}</h4>
                     </div>
-                `)    
-        }
+                `)
+                }
+            } else {
+                var data = response[0].data;
+                var a;
+                // console.log(data);
+                
+                for (a = 0; a < data.length; a++) {
+                    var name = data[a].name;
+                    $(".wrap-3-col").append(`
+                    <div class="cardc">
+                        <h4>${name}</h4>
+                    </div>
+                `)
+                }
+            }
+
+        })
+        var a;
+        // for (a=0; a<response.length; a++) {
+        //     var name = response[a].name;
+        //     $(".wrap-3-col").append(`
+        //             <div class="cardc">
+        //                 <h4>${name}</h4>
+        //             </div>
+        //         `)    
+        // }
     })
 })
