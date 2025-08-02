e14e2021622546a76b8e760b02d13c628c63fab3
Dedi Arief Wibisono <dedi.wibisono@Dedis-MacBook-Pro.local>
2025-08-02 09:59:57 +0700

    update

---
+++ js/contact.js
@@ -0,0 +1,57 @@
+var baseURL = "company-profile-firm"
+
+$.ajax({
+        url: '/'+baseURL+'/js/contact.json',
+        // url: 'js/contact.json',
+        method: 'GET',
+        success: (response => {
+
+            $(".row .loopingx .email, .row .loopingx .address,.subtitle,.subtitlex").html("")
+            $(".click-flag").click(function () {
+                var localS = (localStorage.getItem('lang'));
+                var indonesia = localS == "id";
+                var inggris = localS == "en";
+                
+                if (inggris) {
+                    var data = (response[1].data);
+                    var a;
+                    // console.log(data);
+                    
+                    for (a=0; a<data.length; a++) {
+                        var email = data[a].email;
+                        var address = data[a].address;
+                        var title = data[a].title;
+                        $(".subtitle, .subtitlex").html(title)
+                        $(".loopingx .email").html(`
+                            <a href="${email}">${email}</a>
+                            `)
+
+                        $(".loopingx .address").html(address)    
+                        
+                        
+                    }
+
+                    
+                } else {
+                    var data = (response[0].data);
+                    var a;
+                    // console.log(data);
+                    
+                    for (a=0; a<data.length; a++) {
+                        var email = data[a].email;
+                        var address = data[a].address;
+                        var title = data[a].title;
+                        $(".subtitle, .subtitlex").html(title)
+                        $(".loopingx .email").html(`
+                            <a href="${email}">${email}</a>
+                            `)
+
+                        $(".loopingx .address").html(address)    
+                        
+                        
+                    }
+                }
+            
+            })
+        })
+})
