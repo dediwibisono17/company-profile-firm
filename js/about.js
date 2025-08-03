a5327c36693482f99b0a0f59749aee73e78f5234
Dedi Arief Wibisono <dedi.wibisono@Dedis-MacBook-Pro.local>
2025-08-03 09:08:17 +0700

    update

--- js/about.js
+++ js/about.js
@@ -6,14 +6,9 @@ $.ajax({
     method: 'GET',
     success: (response=>{
         // console.log(response);
-        $(".subtitle span,.subtitlex,.about-content,.quotes").html("");
-        $(".click-flag").click(function () {
-                var localS = (localStorage.getItem('lang'));
-                var indonesia = localS == "id";
-                var inggris = localS == "en";
+        $(".subtitle .dedix,.subtitlex,.about-para,.quotes").html("");
 
-                if (inggris) {
-                    $(".about-content").html("")
+           $(".about-para").html("")
                     $(".quotes").html("")
                     var data = (response[1].data);
                     var a;
@@ -24,15 +19,38 @@ $.ajax({
 
                         var title = data[a].title;
 
-                        $(".subtitle span,.subtitlex").html(title);
-                        $(".about-content").append(desc)
+                        $(".subtitle .dedix,.subtitlex").html(title);
+                        $(".about-para").append(desc)
+                        $(".quotes").append(quote)
+
+                        
+                    }
+        $(".click-flag").click(function () {
+                var localS = (localStorage.getItem('lang'));
+                var indonesia = localS == "id";
+                var inggris = localS == "en";
+
+                if (inggris) {
+                    $(".about-para").html("")
+                    $(".quotes").html("")
+                    var data = (response[1].data);
+                    var a;
+                    for (a=0; a<data.length; a++) {
+                        var desc =data[a].desc;
+                        var quote = data[a].quote;
+                        // console.log(desc);
+
+                        var title = data[a].title;
+
+                        $(".subtitle .dedix,.subtitlex").html(title);
+                        $(".about-para").append(desc)
                         $(".quotes").append(quote)
 
                         
                     }
                     
                 } else {
-                    $(".about-content").html("")
+                    $(".about-para").html("")
                     $(".quotes").html("")
                     // console.log(response[0].lang);
                      var data = (response[0].data);
@@ -44,8 +62,8 @@ $.ajax({
 
                         var title = data[a].title;
 
-                        $(".subtitle,.subtitlex").html(title);
-                        $(".about-content").append(desc)
+                        $(".subtitle .dedix,.subtitlex").html(title);
+                        $(".about-para").append(desc)
                         $(".quotes").append(quote)
 
                         
