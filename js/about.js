7f943adaef190f4b80e01cb61455fc36de528eef
Dedi Arief Wibisono <dedi.wibisono@Dedis-MacBook-Pro.local>
2025-08-08 11:40:34 +0700

    update

--- js/about.js
+++ js/about.js
@@ -11,9 +11,8 @@ $.ajax({
             var localS = (localStorage.getItem('lang'));
             var indonesia = localS == "id";
             var inggris = localS == "en";
-        
-        },10)
-           $(".about-para").html("")
+         if (inggris) {
+                    $(".about-para").html("")
                     $(".quotes").html("")
                     var data = (response[1].data);
                     var a;
@@ -30,6 +29,49 @@ $.ajax({
 
                         
                     }
+                    
+                } else if(indonesia) {
+                    $(".about-para").html("")
+                    $(".quotes").html("")
+                    // console.log(response[0].lang);
+                     var data = (response[0].data);
+                    var a;
+                    for (a=0; a<data.length; a++) {
+                        var desc =data[a].desc;
+                        var quote = data[a].quote;
+                        // console.log(desc);
+
+                        var title = data[a].title;
+
+                        $(".subtitle .dedix,.subtitlex").html(title);
+                        $(".about-para").append(desc)
+                        $(".quotes").append(quote)
+
+                        
+                    }
+                    
+                }
+                else {
+                      $(".about-para").html("")
+                    $(".quotes").html("")
+                    var data = (response[1].data);
+                    var a;
+                    for (a=0; a<data.length; a++) {
+                        var desc =data[a].desc;
+                        var quote = data[a].quote;
+                        // console.log(desc);
+
+                        var title = data[a].title;
+
+                        $(".subtitle .dedix,.subtitlex").html(title);
+                        $(".about-para").append(desc)
+                        $(".quotes").append(quote)
+
+                        
+                    }
+                }
+        },10)
+          
         $(".click-flag").click(function () {
                 var localS = (localStorage.getItem('lang'));
                 var indonesia = localS == "id";
